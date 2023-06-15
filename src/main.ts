dd4b6c49474185328a467553c0c48d90b2e807f0
ThomasP850 <thomas.peterson@hometownticketing.com>
2023-06-15 10:13:56 -0400

    Remove example code from main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { orderCreate } from './routes/order.create';
 
 const app = express();
 const port = 3000;
@@ -7,8 +6,6 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
-app.post('/orders', orderCreate);
 
 const server = app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- src/routes/order.create.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-export const orderCreate = (req: express.Request) => {
-    const {...data} = req.body;
-    console.log(data)
-
-     
-}
