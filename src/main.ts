db3877abbdb029374591488eb6c420239e053ada
ThomasP850 <thomas.peterson@hometownticketing.com>
2023-06-15 16:55:21 -0400

    Add necessary request middleware

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const port = 3000;
 
 app.get('/', (req, res) => {
