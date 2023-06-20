c98e5d44b961a504d5e5121b9dba4c3bba8a27c4
ThomasP850 <thomas.peterson@hometownticketing.com>
2023-06-20 14:16:32 -0400

    function for mock order creation requests

--- src/routes/order-routes/order.create.spec.ts
+++ src/routes/order-routes/order.create.spec.ts
@@ -5,21 +5,13 @@ import { orderCreate } from './order.create';
 import { checkStock } from '../../util/product-utility';
 import { wipeDataStore } from '../../util/storage-utility';
 import { mockResponse } from '../../util/testing-util/mock-response';
+import { mockOrderReq } from '../../util/testing-util/mock-data';
 
 beforeAll(wipeDataStore);
 
 describe('Order Create Route', () => {
     it('should create a new order with the correct values and store it', async () => {
-        const req = {
-            body: {
-                customer: 'Bill',
-                product: 'C',
-                quantity: 105,
-                paymentMethod: PaymentMethod.Credit
-            }
-        } as express.Request
-
-        const orderRes = await orderCreate(req, mockResponse());
+        const orderRes = await orderCreate(mockOrderReq('Bill', 'C', 105), mockResponse());
 
         expect(orderRes).not.toBeNull();
         expect(orderRes.customer).toBe('Bill');
@@ -28,46 +20,19 @@ describe('Order Create Route', () => {
     });
 
     it('should move a credit order directly to awaitingStock', async () => {
-        const req = {
-            body: {
-                customer: 'Bob',
-                product: 'C',
-                quantity: 105,
-                paymentMethod: PaymentMethod.Credit
-            }
-        } as express.Request
-
-        const orderRes = await orderCreate(req, mockResponse());
+        const orderRes = await orderCreate(mockOrderReq('Bob', 'C', 105), mockResponse());
 
         expect(orderRes.status).toBe(OrderStatus.AwaitingStock);
     });
 
     it('an order with physical payment should have a status of awaitingPayment', async () => {
-        const req = {
-            body: {
-                customer: 'Billy',
-                product: 'C',
-                quantity: 35,
-                paymentMethod: PaymentMethod.Physical
-            }
-        } as express.Request
-
-        const orderRes = await orderCreate(req, mockResponse());
+        const orderRes = await orderCreate(mockOrderReq('Billy', 'C', 35, PaymentMethod.Physical), mockResponse());
 
         expect(orderRes.status).toBe(OrderStatus.AwaitingPayment);
     });
 
     it('should ship a product if there is enough stock and decrease stock after', async () => {
-        const req = {
-            body: {
-                customer: 'BillBob',
-                product: 'C',
-                quantity: 35,
-                paymentMethod: PaymentMethod.Credit
-            }
-        } as express.Request
-
-        const orderRes = await orderCreate(req, mockResponse());
+        const orderRes = await orderCreate(mockOrderReq('BillBob', 'C', 35), mockResponse());
 
         expect(orderRes.status).toBe(OrderStatus.Shipped);
         expect(checkStock('C')).resolves.toBe(65);
